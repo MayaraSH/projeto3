be8ff84d605db30aaab5033418d2558c363b6d6b
@MayaraSH <mca.shalom@gmail.com>
2025-07-01 23:27:47 +0200

    Corrigindo validação jQuery

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    $('#telefone').mask('(00) 00000 - 0000', { 
+    $('#telefone').mask('(000) 00000 - 0000', { 
         placeholder: ('(DDD) 12345-6789') 
     });
 
